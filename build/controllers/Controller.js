82824b7814f14367f3c426d37a48b227d9b6f8b7
Tyler Hutchinson <jhutchinson192390@gmail.com>
2023-01-03 01:28:47 -0500

    -

--- build/controllers/Controller.js
+++ build/controllers/Controller.js
@@ -1,20 +1,16 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+'use strict';
+Object.defineProperty(exports, '__esModule', { value: true });
 exports.Controller = void 0;
 class Controller {
-    constructor(route, subControllers = []) {
-        this.subControllers = subControllers;
-        this.route = route;
+  constructor(route, subControllers = []) {
+    this.subControllers = subControllers;
+    this.route = route;
+    console.log(this.subControllers);
+    if (this.subControllers) {
+      console.log(this.route);
+    } else {
+      console.log('root');
     }
-    updateRoute(path = '') {
-        console.log(this);
-        if (this.subControllers) {
-            for (let x of this.subControllers) {
-                x.route = this.route + x.route;
-                x.updateRoute();
-            }
-        }
-        console.log('sub', this.subControllers);
-    }
+  }
 }
 exports.Controller = Controller;
